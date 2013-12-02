d0eb21c3c4941244e18462b2c656b18f842e4bf7
rsivahar <rathaisiva@gmail.com>
2013-12-02 12:05:43 -0500

    Update BloodTest.hpp

--- Lab4/src/BloodTest.hpp
+++ Lab4/src/BloodTest.hpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-//Base Class
+//Base Class: This class has variables and functions that are common between all classes that inherit from this one. 
 class BloodTest{
 protected:
 	string gender;
@@ -20,11 +20,12 @@ public:
 	string member;
 	string ID;
 	string comment;
-    BloodTest();
-    void annotation();
-    void doctorsannotation();
+    BloodTest(); //This is the constructor for the base class.
+    void annotation();//This function allows nurses to add annotations after a test has been added into the system.
+    void doctorsannotation();//This function allows doctors to add an annotation after reviewing test results. 
 };
 
+//This class is for a Blood Count Test. It inherits from the base class above. 
 class BloodCount: public BloodTest{
 protected:
 	float redbloodcells;
@@ -36,21 +37,23 @@ protected:
 	string rbcDisplay,wbcDisplay, pDisplay, hemoDisplay, hDisplay,vDisplay;
 
 public:
-	BloodCount();
-	void bloodTestResults();
-	void bloodCountDisplay();
+	BloodCount(); //This is the constructor for this class. 
+	void bloodTestResults(); //This function asks the user to input the individual test results into the system so that they can be recorded. It then states whether the result is normal or not. 
+	void bloodCountDisplay();//This function displays all the results that were entered into the system from the function above. 
 };
 
+//This class is for a Blood Glucose Test. It inherits from the base class above.
 class BloodGlucose: public BloodTest{
 protected:
 	float results;
 	string condition;
 public:
-	BloodGlucose();
-	string bloodGlucoseResults();
-	void bloodGlucoseDisplay();
+	BloodGlucose();//This is the constructor for this class.
+	string bloodGlucoseResults(); //This function asks the user to input the test result into the system so that they can be recorded. It then states whether the result is normal or not.
+	void bloodGlucoseDisplay();//This function displays the result that was entered into the system from the function above.
 };
 
+//This class is for a Lipoprotein Blood Test. It inherits from the base class above. 
 class Lipoprotein: public BloodTest{
 protected:
 	float totalCholesterol;
@@ -59,9 +62,10 @@ protected:
 	string totalDisplay,hdlDisplay,ldlDisplay;
 
 public:
-	Lipoprotein();
-	void lipoproteinResults();
-	void lipoproteinDisplay();
+	Lipoprotein();//This is the constructor for this class.
+	void lipoproteinResults();//This function asks the user to input the individual test results into the system so that they can be recorded. It then states whether the result is normal or not.
+	void lipoproteinDisplay();//This function displays the results that were entered into the system from the function above.
+};
 };
 
 #endif /* BLOODTEST_HPP_ */
